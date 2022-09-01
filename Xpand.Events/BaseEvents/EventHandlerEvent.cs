61c74bde73a5901f6d24d5ab3ccb57b5aad77dd5
Nikolay Nakorkeshko <shortkedr@gmail.com>
2022-09-01 11:46:12 +0300

    Add EventHandler events

---
+++ Xpand.Events/BaseEvents/EventHandlerEvent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Xpand.Events.Delegates;
+
+namespace Xpand.Events {
+    //TODO implement and correlate names with other XEvent. Make it easy to remember in use of different types of events
+    public class EventHandlerEvent<TSender, TEventArgs> : BaseEvent<EventHandler<TSender, TEventArgs>> 
+        where TEventArgs : EventArgs
+    {
+
+        public void Invoke(TSender sender, TEventArgs args) {
+            if (IsSuspended) return;
+            PrepareInvoke();
+            for (int i = 0; i < _subscriptions.Count; i++) {
+                _subscriptions[i].Invoke(sender, args);
+            }
+        }
+
+    }
+}
---
+++ Xpand.Events/Delegates/EventHandler.cs
@@ -0,0 +1,6 @@
+﻿using System;
+
+namespace Xpand.Events.Delegates {
+    public delegate void EventHandler<in TEventArgs>(TEventArgs args) where TEventArgs : EventArgs;
+    public delegate void EventHandler<in TSender, in TEventArgs>(TSender sender, TEventArgs args) where TEventArgs : EventArgs;
+}
