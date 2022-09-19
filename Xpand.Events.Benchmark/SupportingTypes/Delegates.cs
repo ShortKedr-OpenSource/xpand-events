32432b2ad0fdffd06ba233759e4dece9fd7ad2d7
Nikolay Nakorkeshko <shortkedr@gmail.com>
2022-09-19 18:30:17 +0300

    Remove Benchmark useless types

--- Xpand.Events.Benchmark/SupportingTypes/Delegates.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace Xpand.Events.Benchmark.SupportingTypes {
-    public class Delegates {
-        public delegate void SenderEventHandler(int intArg, float floatArg, double doubleArg, string stringArg);
-    }
-}
--- Xpand.Events.Benchmark/SupportingTypes/Sender.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Xpand.Events.Benchmark.SupportingTypes {
-    public class Sender {
-        public readonly string Name;
-
-        public Sender(string name) {
-            Name = name;
-        }
-    }
-}
